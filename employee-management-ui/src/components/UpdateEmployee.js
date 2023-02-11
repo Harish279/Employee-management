4baca71d76437c2f0c8f5fb23cf365d09db78bea
Harish S G <sgharish279@gmail.com>
2023-02-11 19:06:22 +0530

    Removed sub-module

---
+++ employee-management-ui/src/components/UpdateEmployee.js
@@ -0,0 +1,87 @@
+import React from 'react'
+import { useState,useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import EmployeeService from '../services/EmployeeService';
+
+const UpdateEmployee = () => {
+
+    const {id}=useParams();
+    const navigate=useNavigate();
+    const [employee, setEmployee] = useState({
+        id:id,
+        firstName:"",
+        lastName:"",
+        emailId:""
+    });
+    const updateEmployee=(e)=>{
+        e.preventDefault();
+        EmployeeService.updateEmployee(employee,id).then((response)=>{
+            navigate("/employeeList");
+        })
+        .catch((error)=>{
+            console.log(error);
+        })
+    }
+
+    useEffect(() => {
+        const fetchData=async()=>{
+            try {
+                const response=await EmployeeService.getEmployeeById(id);
+                setEmployee(response.data);
+              } catch (error) {
+                console.log(error);
+              }
+        }
+      fetchData();
+    }, [])
+    
+
+    const handleChange =(e)=>{
+        const value=e.target.value;
+        setEmployee({...employee,[e.target.name]:value});
+    }
+
+  return (
+    <div className='max-w-2xl flex shadow border-b mx-auto'>
+        <div className='px-8 py-8'>
+            <div className='font-thin text-2xl tracking-wider'>
+                Edit Employee
+            </div>
+            <div className='items-center justify-center h-14 w-full my-4'>
+                <label className='block text-gray-600 text-sm font-normal'>First Name</label>
+                <input  type="text" 
+                        className='h-10 w-96 border mt-2 px-2 py-2' 
+                        name='firstName' 
+                        value={employee.firstName}
+                        onChange={(e)=>handleChange(e)}>
+                </input>
+            </div>
+            <div className='items-center justify-center h-14 w-full my-4'>
+                <label className='block text-gray-600 text-sm font-normal'>Last Name</label>
+                <input type="text" 
+                        className='h-10 w-96 border mt-2 px-2 py-2'
+                        name='lastName' 
+                        value={employee.lastName}
+                        onChange={(e)=>handleChange(e)}>
+                 </input>
+            </div>
+            <div className='items-center justify-center h-14 w-full my-4'>
+                <label className='block text-gray-600 text-sm font-normal'>Email</label>
+                <input type="email"
+                        className='h-10 w-96 border mt-2 px-2 py-2'
+                        name='emailId'
+                        value={employee.emailId}
+                        onChange={(e)=>handleChange(e)}>
+                   </input>
+            </div>
+            <div className='items-center justify-center h-14 w-full my-4 space-x-4 pt-4'>
+                <button onClick={updateEmployee} className='rounded text-white font-semibold bg-green-400 py-2 px-6 hover:bg-green-700'>Update</button>
+                <button onClick={()=>navigate("/employeeList")} className='rounded text-white font-semibold bg-red-400 py-2 px-6 hover:bg-red-700'>Cancel</button>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default UpdateEmployee
